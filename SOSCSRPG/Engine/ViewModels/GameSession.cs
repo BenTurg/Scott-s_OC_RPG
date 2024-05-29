f6ac56899acec385bbc8f5ce0215681b586f75cd
@BenTurg <ben9371@gmail.com>
2024-05-29 18:43:21 +0300

    create: Location class & creating world and WorldFactory NOTE: the locations images isnt load to the screen, fix it later

--- SOSCSRPG/Engine/ViewModels/GameSession.cs
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
+        public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
-        
+        public Location CurrentLocation { get; set; }
+
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -20,6 +23,22 @@ namespace Engine.ViewModels
             CurrentPlayer.Gold = 1000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+            /*
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house";
+            //CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+            //CurrentLocation.ImageName = "pack://application:,,,/AssemblyName;/components/Images/Locations/Home.png";
+            //CurrentLocation.ImageName = "/AssemblyName;/components/Images/Locations/Home.png";
+            */
+
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
         }
     }
 }
